15e0d11c1ee0a590060c516826e8d36ac14dbb4b
Administrator <Administrator@WIN-20190424SIS>
2019-06-27 17:11:09 +0800

    springcloot的项目

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,36 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	private Integer id;
+	private String name;
+	private Integer number;
+	public Item(int i, String string, int j) {
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+}
---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
@@ -0,0 +1,27 @@
+package com.tedu.sp01.pojo;
+
+import java.util.List;
+
+public class Order {
+	private String id;
+	private User user;
+	private List<Item> items;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public List<Item> getItems() {
+		return items;
+	}
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+}
